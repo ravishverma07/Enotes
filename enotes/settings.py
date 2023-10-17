f56d575623c1e96a62482dd0481f212c6dc88fda
Ravish Verma <ravishverma070@gmail.com>
2023-10-17 19:33:31 +0530

    added postgresql

--- enotes/settings.py
+++ enotes/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os 
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -110,13 +110,22 @@ WSGI_APPLICATION = 'enotes.wsgi.application'
 LOGIN_REDIRECT_URL = 'google_auth_callback'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}'''
+
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgres://ravish:x8snnaEgvuj60qdLd45rMwtNgI4qCXbP@dpg-ckn66s91rp3c7397a24g-a.singapore-postgres.render.com/postgresql_3wf8'
+    )
 }
+
 
 
 # Password validation
