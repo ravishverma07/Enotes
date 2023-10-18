0b40536d69a636d4de9618742a95b79c3f3b5475
Ravish Verma <ravishverma070@gmail.com>
2023-10-18 15:38:52 +0530

    added env variable

--- enotes/settings.py
+++ enotes/settings.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os 
 import dj_database_url
-import environ
+import importlib
+environ = importlib.import_module('environ')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
