16ad85d09ccb0bb0e3038cefba18966aa24ea123
Ravish Verma <ravishverma070@gmail.com>
2023-10-23 22:38:52 +0530

    changed settings

--- enotes/settings.py
+++ enotes/settings.py
@@ -171,6 +171,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-MEDIA_URL = 'https://enotes-1u3q.onrender.com/media/' 
-MEDIA_ROOT = '/opt/render/project/src/media/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
